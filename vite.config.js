81bc08f81bb1a179eec6704bf14006ed593645ca
Ahmed Ghanim <ghanimdj@gmail.com>
2022-11-13 03:39:27 +0200

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import vuetify from "vite-plugin-vuetify";
 import { transformAssetUrls } from "vite-plugin-vuetify";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
 import VitePluginBrowserSync from "vite-plugin-browser-sync";
+import { splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    cors: true,
     proxy: {
       "/api": {
         target: "https://api.football-data.org/v4/",
@@ -30,10 +32,32 @@ export default defineConfig({
     },
   },
   build: {
-    sourcemap: true,
-    minify: "esbuild",
+    //target: 'es2015',
+    target: "es2017",
+    // minify: "esbuild",
+    cssTarget: "chrome79",
+    minify: "terser",
+    brotliSize: false,
+    chunkSizeWarningLimit: 5000,
+    terserOptions: {
+      compress: {
+        drop_console: false,
+        pure_funcs: ["console.log", "console.info"],
+        drop_debugger: true,
+      },
+    },
+    assetsDir: "static/assets",
+    rollupOptions: {
+      output: {
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+      },
+    },
   },
+  base: "./",
   plugins: [
+    splitVendorChunkPlugin(),
     vue({
       template: { transformAssetUrls },
     }),
